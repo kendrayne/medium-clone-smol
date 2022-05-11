79fcdd3347eff9824ab48726dc1ccf6e64f89b7f
Kendra <kendrayne@gmail.com>
2022-05-11 02:27:29 -0400

    how will i ever get a jawb

--- scripts.js
+++ scripts.js
@@ -1,28 +1,25 @@
-// const sound = document.getElementById("deviruch");
-chrome.webRequest.onHeadersReceived.addListener(
-  (details) => {
-    const responseHeaders = details.responseHeaders.map((item) => {
-      if (item.name.toLowerCase() === "access-control-allow-origin") {
-        item.value = "*";
-      }
-    });
-    return { responseHeaders };
-  },
-  { urls: ["<all_urls>"] },
-  ["blocking", "responseHeaders", "extraHeaders"]
-);
-
+// const sound = getElementsByClassName("deviruch");
 const sound = document.createElement("audio");
-sound.src = "/sounds/deviruchi_idle.mp3"
+sound.src = "https://www.mediafire.com/file/xomgaf15e9vivyk/deviruchi_idle.mp3/file";
 const deviruchi = document.getElementsByClassName("deviruchi");
 // console.log(deviruchi);
-console.log(sound);
+// console.log(sound);
+
 function playSound() {
-  deviruchi.addEventListener("click", (e) => {
-    sound
-      .play()
-      .catch((e) => console.error(e.message, "Now, click here and try again"));
+  deviruchi.addEventListener("mouseonenter", (e) => {
+    try {
+      sound.play();
+    } catch (err) {
+      "please why u no workk"
+    }
+      // .catch((e) => console.error(e.message, "Now, click here and try again"));
   });
+}
+
+function handlePlayButton() {
+  if (sound.paused) {
+    sound.play();
+  }
 }
 // window.onfocus = playSound();
 // window.onfocus = playSound();
