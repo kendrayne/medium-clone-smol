65a497010aefeaa61b5154517e2539631c38983b
Kendra <kendrayne@gmail.com>
2022-05-11 02:02:16 -0400

    pls

--- scripts.js
+++ scripts.js
@@ -1,14 +1,14 @@
 // const sound = document.getElementById("deviruch");
-const sound = "/Users/kendra/Desktop/medium-clone-proj/sounds/deviruchi_idle.mp3"
+const sound =
+  "/Users/kendra/Desktop/medium-clone-proj/sounds/deviruchi_idle.mp3";
 const deviruchi = document.getElementsByClassName("deviruchi");
 
 function playSound() {
-  deviruchi.addEventListener("onmouseover", (e) => {
+  deviruchi.addEventListener("mouseover", (e) => {
     sound
       .play()
       .catch((e) => console.error(e.message, "Now, click here and try again"));
-
-  })
+  });
 }
 // window.onfocus = playSound();
 // window.onfocus = playSound();
