ef9d11b4c619bc6edd3944a99e40aae7314c3859
Kendra <kendrayne@gmail.com>
2022-05-11 02:38:54 -0400

    not even documentation can save me. back to hw ig

--- scripts.js
+++ scripts.js
@@ -3,20 +3,21 @@
 // console.log(deviruchi);
 // console.log(sound);
 
-function playSound() {
-  const sound = document.DOCUMENT_TYPE_NODE("audio");
-  sound.src =
-    "https://www.mediafire.com/file/xomgaf15e9vivyk/deviruchi_idle.mp3/file";
-  const deviruchi = document.getElementsByClassName("deviruchi");
-  deviruchi.addEventListener("mouseonenter", (e) => {
-    try {
-      sound.play();
-    } catch (err) {
-      console.log("please why u no workk")
-    }
+window.addEventListener("DOMContentLoaded", event => {
+
+    const sound = document.createElement("audio");
+  sound.src = "https://www.mediafire.com/file/xomgaf15e9vivyk/deviruchi_idle.mp3/file";
+    const deviruchi = document.getElementsByClassName("deviruchi");
+    deviruchi.addEventListener("mouseonenter", (e) => {
+      try {
+        sound.play();
+      } catch (err) {
+        console.log("please why u no workk")
+      }
       // .catch((e) => console.error(e.message, "Now, click here and try again"));
+    });
   });
-}
+
 // window.onload = playSound;
 
 function handlePlay() {
