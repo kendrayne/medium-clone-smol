cf581427de055ca2b8645a21edac84721d5f2d18
Kendra <kendrayne@gmail.com>
2022-05-11 02:09:31 -0400

    different error now so that's progress?

--- scripts.js
+++ scripts.js
@@ -1,10 +1,24 @@
 // const sound = document.getElementById("deviruch");
-const sound =
-  "/Users/kendra/Desktop/medium-clone-proj/sounds/deviruchi_idle.mp3";
-const deviruchi = document.getElementsByClassName("deviruchi");
+chrome.webRequest.onHeadersReceived.addListener(
+  (details) => {
+    const responseHeaders = details.responseHeaders.map((item) => {
+      if (item.name.toLowerCase() === "access-control-allow-origin") {
+        item.value = "*";
+      }
+    });
+    return { responseHeaders };
+  },
+  { urls: ["<all_urls>"] },
+  ["blocking", "responseHeaders", "extraHeaders"]
+);
 
+const sound = document.createElement("audio");
+sound.src = "/sounds/deviruchi_idle.mp3"
+const deviruchi = document.getElementsByClassName("deviruchi");
+// console.log(deviruchi);
+console.log(sound);
 function playSound() {
-  deviruchi.addEventListener("mouseover", (e) => {
+  deviruchi.addEventListener("click", (e) => {
     sound
       .play()
       .catch((e) => console.error(e.message, "Now, click here and try again"));
