361ac249b09b4bf5fb01bce49e8a65d4cd399900
Kendra <kendrayne@gmail.com>
2022-05-11 02:34:47 -0400

    sleepy

--- scripts.js
+++ scripts.js
@@ -1,16 +1,18 @@
 // const sound = getElementsByClassName("deviruch");
-const sound = document.createElement("audio");
-sound.src = "https://www.mediafire.com/file/xomgaf15e9vivyk/deviruchi_idle.mp3/file";
-const deviruchi = document.getElementsByClassName("deviruchi");
+
 // console.log(deviruchi);
 // console.log(sound);
 
 function playSound() {
+  const sound = document.DOCUMENT_TYPE_NODE("audio");
+  sound.src =
+    "https://www.mediafire.com/file/xomgaf15e9vivyk/deviruchi_idle.mp3/file";
+  const deviruchi = document.getElementsByClassName("deviruchi");
   deviruchi.addEventListener("mouseonenter", (e) => {
     try {
       sound.play();
     } catch (err) {
-      "please why u no workk"
+      console.log("please why u no workk")
     }
       // .catch((e) => console.error(e.message, "Now, click here and try again"));
   });
